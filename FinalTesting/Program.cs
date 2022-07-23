f6b85b9f1638d4bbf3f311ac922d79ff7689cb4a
David Anthony Le <bebaoga123@gmail.com>
2022-07-23 17:22:48 +0700

    20

--- FinalTesting/Program.cs
+++ FinalTesting/Program.cs
@@ -37,7 +37,7 @@ namespace FinalTesting
             Console.WriteLine("|        Menu:      |");
             Console.WriteLine("+---------+---------+");
             Console.BackgroundColor = ConsoleColor.Red;
-            Console.WriteLine("1.Nhap lai day so");
+            Console.WriteLine("> 1.Nhap lai day so");
             Console.BackgroundColor = ConsoleColor.White;
             Console.WriteLine("2.Sap xep tang dan");
             Console.WriteLine("3.Sap xep giam dan");
@@ -52,12 +52,12 @@ namespace FinalTesting
             Console.WriteLine("+---------+---------+");
             Console.WriteLine("|        Menu:      |");
             Console.WriteLine("+---------+---------+");
-            Console.WriteLine("1.Nhap lai day so");
-            Console.WriteLine("2.Sap xep tang dan");
-            Console.WriteLine("3.Sap xep giam dan");
-            Console.WriteLine("4.In ket qua");
-            Console.WriteLine("5.Demo sx tang dan");
-            Console.WriteLine("6.Thoat");
+            Console.WriteLine("1.Nhap lai day so  ");
+            Console.WriteLine("2.Sap xep tang dan  ");
+            Console.WriteLine("3.Sap xep giam dan  ");
+            Console.WriteLine("4.In ket qua  ");
+            Console.WriteLine("5.Demo sx tang dan  ");
+            Console.WriteLine("6.Thoat  ");
             Console.WriteLine("+---------+---------+");
         }
         private static int Y = 3;
@@ -148,30 +148,30 @@ namespace FinalTesting
                             case 4:
                                 Order = 0;
                                 Sort(Order);
-                                Console.SetCursorPosition(x + 25, Y);
-                                Console.Write("SORT COMPLETED! hit enter to delete");
-                                Console.ReadLine();
-                                Console.SetCursorPosition(x + 25, Y);
-                                Console.ForegroundColor = ConsoleColor.White;
-                                Console.Write("SORT COMPLETED! hit enter to delete");
-                                Console.ForegroundColor = ConsoleColor.Black;
+                                tempClear();
                                 break;
                             case 5:
                                 Order = 1;
                                 Sort(Order);
-                                Console.SetCursorPosition(x + 25, Y);
-                                Console.Write("SORT COMPLETED! hit enter to delete");
-                                Console.ReadLine();
-                                Console.SetCursorPosition(x + 25, Y);
-                                Console.ForegroundColor = ConsoleColor.White;
-                                Console.Write("SORT COMPLETED! hit enter to delete");
-                                Console.ForegroundColor = ConsoleColor.Black;
+                                tempClear();
                                 break;
                             case 6:
                                 Print();
+                                Console.ReadLine();
+                                Cleanup();
+                                Console.SetCursorPosition(x, Y);
+                                Console.ForegroundColor = ConsoleColor.Black;
+                                Console.BackgroundColor = ConsoleColor.Red;
+                                Console.WriteLine("> 4.In ket qua");
+                                Console.BackgroundColor = ConsoleColor.White;
+
                                 break;
                             case 7:
-
+                                Demo();
+                                Console.ReadLine();
+                                Console.Clear();
+                                MenuOrginal();
+                                Y = 3;
                                 break;
                             case 8:
                                 Environment.Exit(0);
@@ -181,6 +181,44 @@ namespace FinalTesting
                 }
             }
         }
+        private static void Cleanup()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            int plus = 0;
+
+            for (int i = 0; i < m1.Count; i++)
+            {
+                Console.SetCursorPosition(x + 20 + plus, Y - 3);
+                Console.WriteLine("+----+");
+                Console.SetCursorPosition(x + 20 + plus, Y - 2);
+                Console.WriteLine("| " + m1[i] + "  |");
+                Console.SetCursorPosition(x + 20 + plus, Y - 1);
+                Console.WriteLine("+----+");
+                plus += 6;
+            }
+            plus = 0;
+
+            for (int i = 0; i < m1.Count; i++)
+            {
+                Console.SetCursorPosition(x + 20 + plus, Y);
+                Console.WriteLine("+----+");
+                Console.SetCursorPosition(x + 20 + plus, Y + 1);
+                Console.WriteLine("| " + sorted[i] + "  |");
+                Console.SetCursorPosition(x + 20 + plus, Y + 2);
+                Console.WriteLine("+----+");
+                plus += 6;
+            }
+        }
+        private static void tempClear()
+        {
+            Console.SetCursorPosition(x + 25, Y);
+            Console.Write("SORT COMPLETED! hit enter to delete");
+            Console.ReadLine();
+            Console.SetCursorPosition(x + 25, Y);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("SORT COMPLETED! hit enter to delete");
+            Console.ForegroundColor = ConsoleColor.Black;
+        }
 
         private static void UpDown(int Y)
         {
@@ -189,22 +227,22 @@ namespace FinalTesting
             switch(Y)
             {
                 case 3:
-                    Console.WriteLine("1.Nhap lai day so");
+                    Console.WriteLine("> 1.Nhap lai day so");
                     break;
                 case 4:
-                    Console.WriteLine("2.Sap xep tang dan");
+                    Console.WriteLine("> 2.Sap xep tang dan");
                     break;
                 case 5:
-                    Console.WriteLine("3.Sap xep giam dan");
+                    Console.WriteLine("> 3.Sap xep giam dan");
                     break;
                 case 6:
-                    Console.WriteLine("4.In ket qua");
+                    Console.WriteLine("> 4.In ket qua");
                     break;
                 case 7:
-                    Console.WriteLine("5.Demo sx tang dan");
+                    Console.WriteLine("> 5.Demo sx tang dan");
                     break;
                 case 8:
-                    Console.WriteLine("6.Thoat");
+                    Console.WriteLine("> 6.Thoat");
                     break;
             }
             Console.BackgroundColor = ConsoleColor.White;
@@ -281,18 +319,100 @@ namespace FinalTesting
                 }
             }  
         }
-        private static void Print()
+
+        private static void Demo()
         {
+            for (int i = 0; i < m1.Count; i++)
+            {
+                sorted[i] = Convert.ToInt32(m1[i]);
+            }
             int plus = 0;
             for (int i = 0; i < m1.Count; i++)
             {
-                Console.SetCursorPosition(x + 20 + plus +i, Y);
+                Console.SetCursorPosition(x + 20 + plus, Y - 3);
                 Console.WriteLine("+----+");
-                Console.SetCursorPosition(x + 20 + plus + i, Y + 1);
-                Console.WriteLine(sorted[i] + " | ");
-                Console.SetCursorPosition(x + 20 + plus + i, Y + 2);
+                Console.SetCursorPosition(x + 20 + plus, Y - 2);
+                Console.WriteLine("| " + m1[i] + "  |");
+                Console.SetCursorPosition(x + 20 + plus, Y - 1);
                 Console.WriteLine("+----+");
-                plus 
+                plus += 6;
+            }
+            plus = 0;
+
+            for (int i = 0; i < m1.Count; i++)
+            {
+                Console.SetCursorPosition(x + 20 + plus, Y);
+                Console.WriteLine("+----+");
+                Console.SetCursorPosition(x + 20 + plus, Y + 1);
+                Console.WriteLine("| " + sorted[i] + "  |");
+                Console.SetCursorPosition(x + 20 + plus, Y + 2);
+                Console.WriteLine("+----+");
+                plus += 6;
+            }
+            Console.SetCursorPosition(x + 35, y + 3);
+            Console.Write("Sorting");
+            for (int i = 0; i < 4; i++)
+            {
+                Console.SetCursorPosition(x + 42 + i, y + 3);
+                Console.Write(".");
+                Thread.Sleep(1000);
+            }
+            Console.SetCursorPosition(x + 35, y + 3);
+            Console.Write("Sorting Done!");
+
+            for (int i = 0; i < m1.Count; i++)
+            {
+                for (int j = i; j < m1.Count; j++)
+                {
+                    if (sorted[i] > sorted[j])
+                    {
+                        int tmp = sorted[i];
+
+                        sorted[i] = sorted[j];
+
+                        sorted[j] = tmp;
+                    }
+                    plus = 0;
+                    for (int k = 0; k < m1.Count; k++)
+                    {
+                        Console.SetCursorPosition(x + 20 + plus, Y);
+                        Console.WriteLine("+----+");
+                        Console.SetCursorPosition(x + 20 + plus, Y + 1);
+                        Console.WriteLine("| " + sorted[k] + "  |");
+                        Console.SetCursorPosition(x + 20 + plus, Y + 2);
+                        Console.WriteLine("+----+");
+                        plus += 6;
+                    }
+                }
+                Thread.Sleep(1700);
+            }
+            
+        }
+        private static void Print()
+        {
+            int plus = 0;
+
+            for (int i = 0; i < m1.Count; i++)
+            {
+                Console.SetCursorPosition(x + 20 + plus, Y - 3);
+                Console.WriteLine("+----+");
+                Console.SetCursorPosition(x + 20 + plus, Y - 2);
+                Console.WriteLine("| " + m1[i] + "  |");
+                Console.SetCursorPosition(x + 20 + plus, Y - 1);
+                Console.WriteLine("+----+");
+                plus += 6;
+            }
+            plus = 0;
+
+            for (int i = 0; i < m1.Count; i++)
+            {
+                Console.SetCursorPosition(x + 20 + plus , Y);
+                Console.WriteLine("+----+");
+                Console.SetCursorPosition(x + 20 + plus , Y + 1);
+                Console.WriteLine("| "+ sorted[i] + "  |");
+                Console.SetCursorPosition(x + 20 + plus , Y + 2);
+                Console.WriteLine("+----+");
+                plus += 6;
             }
         }
     }
